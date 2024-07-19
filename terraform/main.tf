ec91fac7ce010b2c7bd791b5aac2734b635f6296
tonylizza <tony.lizza@gmail.com>
2024-07-19 16:01:10 -0400

    Fix for main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,5 +21,5 @@ resource "aws_db_instance" "example" {
 }
 
 output "rds_endpoint" {
-  value = regex("^([^:]+)", aws_db_instance..example.endpoint)
+  value = regex("^([^:]+)", aws_db_instance.example.endpoint)
 }
